9178f5c1f556ea8752db35379b78b3b8a5157bdb
louisbl <louis@beltramo.me>
2015-02-03 11:05:22 +0100

    Display note in details activity

--- app/src/main/java/crm/gobelins/takenote/notes/NoteDetailsActivity.java
+++ app/src/main/java/crm/gobelins/takenote/notes/NoteDetailsActivity.java
@@ -1,7 +1,27 @@
 package crm.gobelins.takenote.notes;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import crm.gobelins.takenote.R;
 
 public class NoteDetailsActivity extends ActionBarActivity {
     public static final String NOTE_ID = "noteId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_details_note);
+
+        int noteId = getIntent().getIntExtra(NOTE_ID, 0);
+        Note note = NotesManager.getInstance().getNoteById(noteId);
+
+        TextView title = (TextView) findViewById(R.id.note_details_title);
+        TextView text = (TextView) findViewById(R.id.note_details_text);
+
+        title.setText(note.getTitle());
+        text.setText(note.getText());
+    }
 }
--- app/src/main/java/crm/gobelins/takenote/notes/NotesManager.java
+++ app/src/main/java/crm/gobelins/takenote/notes/NotesManager.java
@@ -1,16 +1,22 @@
 package crm.gobelins.takenote.notes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class NotesManager {
     private static NotesManager sInstance;
     private List<Note> mNotes;
+    private HashMap<Integer, Note> mNotesByID;
 
     private NotesManager() {
         mNotes = new ArrayList<>();
+        mNotesByID = new HashMap<>();
+
         for (int i = 0; i < 255; i += 1) {
-            mNotes.add(new Note(i, "title " + i, "text " + i));
+            Note note = new Note(i, "title " + i, "text " + i);
+            mNotes.add(note);
+            mNotesByID.put(i, note);
         }
     }
 
@@ -23,5 +29,9 @@ public class NotesManager {
 
     public List<Note> getAllNotes() {
         return mNotes;
+    }
+
+    public Note getNoteById(int ID) {
+        return mNotesByID.get(ID);
     }
 }
