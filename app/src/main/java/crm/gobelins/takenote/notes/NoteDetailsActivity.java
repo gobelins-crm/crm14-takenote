a889c1d47c9aad2e591b75ad4e1cbab5c666b6cc
louisbl <louis@beltramo.me>
2015-02-03 10:45:08 +0100

    Pass Note id to Details Activity with intent extra

---
+++ app/src/main/java/crm/gobelins/takenote/notes/NoteDetailsActivity.java
@@ -0,0 +1,7 @@
+package crm.gobelins.takenote.notes;
+
+import android.support.v7.app.ActionBarActivity;
+
+public class NoteDetailsActivity extends ActionBarActivity {
+    public static final String NOTE_ID = "noteId";
+}
--- app/src/main/java/crm/gobelins/takenote/notes/NotesActivity.java
+++ app/src/main/java/crm/gobelins/takenote/notes/NotesActivity.java
@@ -1,7 +1,11 @@
 package crm.gobelins.takenote.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,6 +15,8 @@ import crm.gobelins.takenote.R;
 
 public class NotesActivity extends ActionBarActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +24,28 @@ public class NotesActivity extends ActionBarActivity {
         // On récupère le layout et l'utilise comme vue de l'activity
         setContentView(R.layout.activity_notes);
 
-        // On récupère une référence vers notre listView
-        ListView list = (ListView) findViewById(R.id.list_notes);
-
         // On récupère un tableau de données en dur
         List<Note> datas = NotesManager.getInstance().getAllNotes();
-
         // On créé l'adapter pour notre listView
-        ArrayAdapter<Note> listAdapter = new NoteAdapter(this, datas);
+        final ArrayAdapter<Note> listAdapter = new NoteAdapter(this, datas);
 
+        // On récupère une référence vers notre listView
+        ListView list = (ListView) findViewById(R.id.list_notes);
         // On lie l'adapter avec la liste
         list.setAdapter(listAdapter);
+
+        // on ajoute l'écouteur pour les click sur les items de la liste
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Note note = listAdapter.getItem(position);
+                Log.i(TAG, String.valueOf(note.getID()));
+
+                Intent intent = new Intent(NotesActivity.this, NoteDetailsActivity.class);
+                intent.putExtra(NoteDetailsActivity.NOTE_ID, note.getID());
+
+                startActivity(intent);
+            }
+        });
     }
 }
