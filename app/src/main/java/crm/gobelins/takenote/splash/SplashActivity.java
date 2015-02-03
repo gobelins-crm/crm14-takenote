04c4a5a394fddddbf998752fff048ea4e4f275cf
louisbl <louis@beltramo.me>
2015-02-03 09:32:24 +0100

    Add colors and appInfo

--- app/src/main/java/crm/gobelins/takenote/splash/SplashActivity.java
+++ app/src/main/java/crm/gobelins/takenote/splash/SplashActivity.java
@@ -2,8 +2,10 @@ package crm.gobelins.takenote.splash;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import crm.gobelins.takenote.R;
 import crm.gobelins.takenote.notes.NotesActivity;
@@ -15,6 +17,13 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
 
         setContentView(R.layout.activity_splash);
 
